93879a8f7249690b658016dcaf236de689741c01
adnvgzz <pkrysiak2.cw@solventum.com>
2026-08-14 09:33:12 +0200

    Health

--- AiDeveloperLab/AiDeveloperLab/Program.cs
+++ AiDeveloperLab/AiDeveloperLab/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHealthChecks();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,6 +21,9 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.MapHealthChecks("/health");
 app.MapControllers();
 
 app.Run();
+
+public partial class Program;
---
+++ README.md
@@ -0,0 +1,5 @@
+# AiDeveloperLab
+
+## Health endpoint
+
+`GET /health` reports the application's health. It returns `200 OK` when all registered health checks are healthy, and `503 Service Unavailable` when one or more checks are unhealthy.
